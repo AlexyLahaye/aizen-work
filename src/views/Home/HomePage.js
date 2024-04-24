a6810eaa421615365c5be44c4806c794b4180a92
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-04-24 09:38:01 +0200

    KAN-7 Création d'un header, Body, Footer (rudimentaire)

---
+++ src/views/Home/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6">
+          {/* <img src="chemin/vers/votre/image.jpg" className="img-fluid" alt="Image d'accueil" /> */}
+        </div>
+        <div className="col-md-6">
+          <div className="text-center">
+            <p>
+              C'est un site qui cherche à vendre des statues/figurines faites à la main.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
