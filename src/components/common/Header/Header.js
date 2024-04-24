8406e2219b1e54256e985d850d81aa44c9c052f7
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-04-24 15:47:28 +0200

    KAN-14 Gérer le design du footer

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Form, Button, NavDropdown } from 'react-bootstrap';
+import theme from '../../../assets/themes/theme';
 
 function Header({ darkMode, toggleTheme }) {
+    const navBarBG = {backgroundColor: darkMode ? theme.dark.background : theme.light.background};
+    const navBarCT = {color: darkMode ? theme.dark.text : theme.light.text,}
+      
   return (
-    <Navbar expand="lg p-2" fixed="top" bg={darkMode ? 'dark' : 'light'} variant={darkMode ? 'dark' : 'light'}>
-      <Navbar.Brand as={Link} to="/">Aizen Work</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
+    <Navbar style={navBarBG} expand="lg p-2" fixed="top">
+    <Navbar.Brand style={navBarCT} as={Link} to="/">Aizen Work</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
         <Nav className="me-auto" navbarScroll>
-          <Nav.Link as={Link} to="/">Accueil</Nav.Link>
-          <Nav.Link as={Link} to="/Galerie">Galerie</Nav.Link>
-          <NavDropdown title="Liscences" id="navbarScrollingDropdown">
+        <Nav.Link style={navBarCT} as={Link} to="/">Accueil</Nav.Link>
+        <Nav.Link style={navBarCT} as={Link} to="/Galerie">Galerie</Nav.Link>
+        <NavDropdown style={navBarCT} title="Liscences" id="navbarScrollingDropdown">
             <NavDropdown.Item href="#idLiscence">One Piece</NavDropdown.Item>
-          </NavDropdown>
+        </NavDropdown>
         </Nav>
         <Form className="d-flex me-2">
-          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-          <Button variant={darkMode ? 'light' : 'dark'}>Recherche</Button>
+        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+        <Button variant={darkMode ? 'light' : 'dark'}>Recherche</Button>
         </Form>
         <Button onClick={toggleTheme} variant={darkMode ? 'light' : 'dark'}>
-          {darkMode ? 'Mode Clair' : 'Mode Sombre'}
+        {darkMode ? 'Mode Clair' : 'Mode Sombre'}
         </Button>
-      </Navbar.Collapse>
+    </Navbar.Collapse>
     </Navbar>
   );
 }
